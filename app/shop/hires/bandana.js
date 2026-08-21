2d2924794b8ccf9e22f5c0ca5b5f62d0d4a191ad
bigstock2001 <donald.l.dunn@gmail.com>
2026-08-21 18:14:33 -0500

    Assemble high-resolution bandana image source

---
+++ app/shop/hires/bandana.js
@@ -0,0 +1,9 @@
+import b0 from "./bandana0";
+import b1 from "./bandana1";
+import b2 from "./bandana2";
+import b3 from "./bandana3";
+import b4 from "./bandana4";
+
+const bandanaHiRes = `data:image/webp;base64,${b0}${b1}${b2}${b3}${b4}`;
+
+export default bandanaHiRes;
