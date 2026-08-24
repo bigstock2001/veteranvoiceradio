290cb7680b6a885b3c83d03b614ee9dc881b522e
bigstock2001 <donald.l.dunn@gmail.com>
2026-08-24 17:18:24 -0500

    Add Meta Pixel base tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID?.trim();
+
   return (
     <html lang="en">
       <head>
@@ -33,6 +35,23 @@ export default function RootLayout({
             gtag('config', 'G-RLQ6D8W4M5');
           `}
         </Script>
+
+        {metaPixelId ? (
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${metaPixelId}');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+        ) : null}
       </head>
 
       <body>
